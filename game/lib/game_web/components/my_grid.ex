7474621d7f490c56904563d187db0cbbb1ac5fb5
Brandon McClure <brandonmcclure89@gmail.com>
2023-04-29 22:29:34 -0600

    remove grid function move the board state into the live .ex directly - this was needed because I could not pass a list of maps into the component

--- game/lib/game_web/components/my_grid.ex
+++ game/lib/game_web/components/my_grid.ex
@@ -1,65 +1,5 @@
 defmodule GameWeb.MyGrid do
   use Phoenix.Component
-  @doc """
-  Do a thing. My thing
-
-  ## Examples
-
-      <.my_grid id="playfield">
-        This is a grid.
-      </.my_grid>
-
-
-
-  """
-  attr :title,:string, required: true
-  attr :grid_x,:integer, default: 6
-  attr :grid_y,:integer, default: 6
-  attr :rest, :global, include: ~w(form)
-  slot :inner_block, required: true
-  def grid(assigns) do
-
-    ~H"""
-    <div style={"display:grid;  grid-gap:10px;  grid-template-columns:repeat("<>(@grid_x |> Integer.to_string())<>", [col] auto);  grid-template-rows:repeat("<>(@grid_y|> Integer.to_string())<>", [row] auto);  background-color:#fff;  color:#444;>"}>
-      <GameWeb.MyGrid.grid_cell grid_x={1} grid_y={1} cell_type={"start"} />
-      <GameWeb.MyGrid.grid_cell grid_x={1} grid_y={2} cell_type={"road"} />
-      <GameWeb.MyGrid.grid_cell grid_x={1} grid_y={3} cell_type={"road"} />
-      <GameWeb.MyGrid.grid_cell grid_x={1} grid_y={4} cell_type={"road"} />
-      <GameWeb.MyGrid.grid_cell grid_x={1} grid_y={5} cell_type={"road"} />
-      <GameWeb.MyGrid.grid_cell grid_x={1} grid_y={6} cell_type={"border"} />
-      <GameWeb.MyGrid.grid_cell grid_x={2} grid_y={1} cell_type={"border"} />
-      <GameWeb.MyGrid.grid_cell grid_x={2} grid_y={2} cell_type={"road"} />
-      <GameWeb.MyGrid.grid_cell grid_x={2} grid_y={3} cell_type={"road"} />
-      <GameWeb.MyGrid.grid_cell grid_x={2} grid_y={4} cell_type={"road"} />
-      <GameWeb.MyGrid.grid_cell grid_x={2} grid_y={5} cell_type={"road"} />
-      <GameWeb.MyGrid.grid_cell grid_x={2} grid_y={6} cell_type={"border"} />
-      <GameWeb.MyGrid.grid_cell grid_x={3} grid_y={1} cell_type={"border"} />
-      <GameWeb.MyGrid.grid_cell grid_x={3} grid_y={2} cell_type={"road"} />
-      <GameWeb.MyGrid.grid_cell grid_x={3} grid_y={3} cell_type={"road"} />
-      <GameWeb.MyGrid.grid_cell grid_x={3} grid_y={4} cell_type={"road"} />
-      <GameWeb.MyGrid.grid_cell grid_x={3} grid_y={5} cell_type={"road"} />
-      <GameWeb.MyGrid.grid_cell grid_x={3} grid_y={6} cell_type={"border"} />
-      <GameWeb.MyGrid.grid_cell grid_x={4} grid_y={1} cell_type={"border"} />
-      <GameWeb.MyGrid.grid_cell grid_x={4} grid_y={2} cell_type={"road"} />
-      <GameWeb.MyGrid.grid_cell grid_x={4} grid_y={3} cell_type={"road"} />
-      <GameWeb.MyGrid.grid_cell grid_x={4} grid_y={4} cell_type={"road"} />
-      <GameWeb.MyGrid.grid_cell grid_x={4} grid_y={5} cell_type={"road"} />
-      <GameWeb.MyGrid.grid_cell grid_x={4} grid_y={6} cell_type={"border"} />
-      <GameWeb.MyGrid.grid_cell grid_x={5} grid_y={1} cell_type={"border"} />
-      <GameWeb.MyGrid.grid_cell grid_x={5} grid_y={2} cell_type={"road"} />
-      <GameWeb.MyGrid.grid_cell grid_x={5} grid_y={3} cell_type={"road"} />
-      <GameWeb.MyGrid.grid_cell grid_x={5} grid_y={4} cell_type={"road"} />
-      <GameWeb.MyGrid.grid_cell grid_x={5} grid_y={5} cell_type={"road"} />
-      <GameWeb.MyGrid.grid_cell grid_x={5} grid_y={6} cell_type={"border"} />
-      <GameWeb.MyGrid.grid_cell grid_x={6} grid_y={1} cell_type={"border"} />
-      <GameWeb.MyGrid.grid_cell grid_x={6} grid_y={2} cell_type={"road"} />
-      <GameWeb.MyGrid.grid_cell grid_x={6} grid_y={3} cell_type={"road"} />
-      <GameWeb.MyGrid.grid_cell grid_x={6} grid_y={4} cell_type={"road"} />
-      <GameWeb.MyGrid.grid_cell grid_x={6} grid_y={5} cell_type={"road"} />
-      <GameWeb.MyGrid.grid_cell grid_x={6} grid_y={6} cell_type={"end"} />
-    </div>
-    """
-  end
   attr :grid_x,:integer, required: true
   attr :grid_y,:integer, required: true
   attr :cell_type, :string, required: true
@@ -89,15 +29,15 @@ defmodule GameWeb.MyGrid do
     css_class="text-black hover:bg-blue-800 rounded p-3"
     css_class = case {assigns.cell_type} do
       {"road"} -> "text-black"
-      {"start"} -> "text-black hover:bg-green-800"
+      {"start"} -> "text-black hover:bg-green-800 rounded p-3"
       {"border"} -> "text-black"
       {"end"} -> "text-black hover:bg-red-800"
     end
     assigns = assign(assigns, css_class: css_class)
 
-    dbg(assigns.cell_type)
-    dbg(assigns.imgURL)
-    dbg(assigns.cell_type == "end")
+    # dbg(assigns.cell_type)
+    # dbg(assigns.imgURL)
+    # dbg(assigns.cell_type == "end")
     # dbg(assigns)
     style = ~s("grid-column:#{assigns.grid_x}; grid-row:#{assigns.grid_y}")
     assigns = assign(assigns, style: style)
--- game/lib/game_web/live/board_live.ex
+++ game/lib/game_web/live/board_live.ex
@@ -9,6 +9,46 @@ defmodule GameWeb.BoardLive do
      socket
      |> assign(:score, 0)
      |> assign(:page_title, "Delivery - Map 1")
+     |> assign(:board_status, [
+      %{"x" => 1, "y" => 1, "cell_type" => "start", "status" => 1 },
+      %{"x" => 1, "y" => 2, "cell_type" => "road", "status" => 1 },
+      %{"x" => 1, "y" => 3, "cell_type" => "road", "status" => 1 },
+      %{"x" => 1, "y" => 4, "cell_type" => "road", "status" => 1 },
+      %{"x" => 1, "y" => 5, "cell_type" => "road", "status" => 1 },
+      %{"x" => 1, "y" => 6, "cell_type" => "border", "status" => 1 },
+      %{"x" => 2, "y" => 1, "cell_type" => "border", "status" => 1 },
+      %{"x" => 2, "y" => 2, "cell_type" => "road", "status" => 1 },
+      %{"x" => 2, "y" => 3, "cell_type" => "road", "status" => 1 },
+      %{"x" => 2, "y" => 4, "cell_type" => "road", "status" => 1 },
+      %{"x" => 2, "y" => 5, "cell_type" => "road", "status" => 1 },
+      %{"x" => 2, "y" => 6, "cell_type" => "border", "status" => 1 },
+      %{"x" => 3, "y" => 1, "cell_type" => "border", "status" => 1 },
+      %{"x" => 3, "y" => 2, "cell_type" => "road", "status" => 1 },
+      %{"x" => 3, "y" => 3, "cell_type" => "road", "status" => 1 },
+      %{"x" => 3, "y" => 4, "cell_type" => "road", "status" => 1 },
+      %{"x" => 3, "y" => 5, "cell_type" => "road", "status" => 1 },
+      %{"x" => 3, "y" => 6, "cell_type" => "border", "status" => 1 },
+      %{"x" => 4, "y" => 1, "cell_type" => "border", "status" => 1 },
+      %{"x" => 4, "y" => 2, "cell_type" => "road", "status" => 1 },
+      %{"x" => 4, "y" => 3, "cell_type" => "road", "status" => 1 },
+      %{"x" => 4, "y" => 4, "cell_type" => "road", "status" => 1 },
+      %{"x" => 4, "y" => 5, "cell_type" => "road", "status" => 1 },
+      %{"x" => 4, "y" => 6, "cell_type" => "border", "status" => 1 },
+      %{"x" => 5, "y" => 1, "cell_type" => "border", "status" => 1 },
+      %{"x" => 5, "y" => 2, "cell_type" => "road", "status" => 1 },
+      %{"x" => 5, "y" => 3, "cell_type" => "road", "status" => 1 },
+      %{"x" => 5, "y" => 4, "cell_type" => "road", "status" => 1 },
+      %{"x" => 5, "y" => 5, "cell_type" => "road", "status" => 1 },
+      %{"x" => 5, "y" => 6, "cell_type" => "border", "status" => 1 },
+      %{"x" => 6, "y" => 1, "cell_type" => "border", "status" => 1 },
+      %{"x" => 6, "y" => 2, "cell_type" => "road", "status" => 1 },
+      %{"x" => 6, "y" => 3, "cell_type" => "road", "status" => 1 },
+      %{"x" => 6, "y" => 4, "cell_type" => "road", "status" => 1 },
+      %{"x" => 6, "y" => 5, "cell_type" => "road", "status" => 1 },
+      %{"x" => 6, "y" => 6, "cell_type" => "end", "status" => 1 }
+
+     ]
+      )
     }
   end
   def incScore(score) do
@@ -16,10 +56,17 @@ defmodule GameWeb.BoardLive do
   end
   @impl true
   def handle_event("delivery", _params, socket) do
-    dbg("handle delivery")
+    # dbg("handle delivery")
 
     new_socket = socket
       |> assign(:score, incScore(socket.assigns.score))
+
+    {:noreply, new_socket}
+  end
+  @impl true
+  def handle_event("road_rotate", _params, socket) do
+
+    new_socket = socket
 
     {:noreply, new_socket}
   end
